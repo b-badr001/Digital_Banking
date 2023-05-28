bbe03b11961fb0930d6da7d35ff01e267b26c996
b-badr001 <b_ajlil@etu.enset-media.ac.ma>
2023-05-28 13:20:51 +0100

    My first commit

---
+++ src/main/java/ma/enset/digital_banking/DigitalBankingApplication.java
@@ -0,0 +1,72 @@
+package ma.enset.digital_banking;
+
+import ma.enset.digital_banking.entities.AccountOperation;
+import ma.enset.digital_banking.entities.CurrentAccount;
+import ma.enset.digital_banking.entities.Customer;
+import ma.enset.digital_banking.entities.SavingAccount;
+import ma.enset.digital_banking.enums.AccountStatus;
+import ma.enset.digital_banking.enums.OperationType;
+import ma.enset.digital_banking.repositories.AccountOperationRepository;
+import ma.enset.digital_banking.repositories.BankAccountRepository;
+import ma.enset.digital_banking.repositories.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
+import java.util.UUID;
+import java.util.stream.Stream;
+
+@SpringBootApplication
+public class DigitalBankingApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(DigitalBankingApplication.class, args);
+    }
+    @Bean
+    CommandLineRunner start(CustomerRepository customerRepository,
+                            BankAccountRepository bankAccountRepository,
+                            AccountOperationRepository accountOperationRepository){
+        return args ->{
+            Stream.of("Ayman","Hamza","Noha").forEach(name->{
+                Customer customer = new Customer();
+                customer.setName(name);
+                customer.setEmail(name+"@gmail.com");
+                customerRepository.save(customer);
+            });
+            customerRepository.findAll().forEach(cust ->{
+                CurrentAccount currentAccount = new CurrentAccount();
+                currentAccount.setId(UUID.randomUUID().toString());
+                currentAccount.setBalance(Math.random()*90000);
+                currentAccount.setCreatedAt(new Date());
+                currentAccount.setStatus(AccountStatus.CREATED);
+                currentAccount.setCustomer(cust);
+                currentAccount.setOverDraft(9000);
+                bankAccountRepository.save(currentAccount);
+
+                SavingAccount savingAccount = new SavingAccount();
+                savingAccount.setId(UUID.randomUUID().toString());
+                savingAccount.setBalance(Math.random()*90000);
+                savingAccount.setCreatedAt(new Date());
+                savingAccount.setStatus(AccountStatus.CREATED);
+                savingAccount.setCustomer(cust);
+                savingAccount.setInterestRate(5.5);
+                bankAccountRepository.save(savingAccount);
+            } );
+
+            bankAccountRepository.findAll().forEach(acc->{
+                for(int i = 0; i<10;i++){
+                    AccountOperation accountOperation = new AccountOperation();
+                    accountOperation.setOperationDate(new Date());
+                    accountOperation.setAmount(Math.random()*12000);
+                    accountOperation.setType(Math.random()>0.5? OperationType.DEBIT: OperationType.CREDIT);
+                    accountOperation.setBankAccount(acc);
+                    accountOperationRepository.save(accountOperation);
+                }
+            });
+        };
+    }
+
+}
---
+++ src/main/java/ma/enset/digital_banking/repositories/AccountOperationRepository.java
@@ -0,0 +1,7 @@
+package ma.enset.digital_banking.repositories;
+
+import ma.enset.digital_banking.entities.AccountOperation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccountOperationRepository extends JpaRepository<AccountOperation,Long> {
+}
